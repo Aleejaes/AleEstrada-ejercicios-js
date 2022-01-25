dd37440dcb6d743b6d275fa1879dc7a5bc7711e9
Aleejaes <ada_hack364@hackademy.mx>
2022-01-25 16:47:46 -0600

    Agregue solución 2-3

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+
+const nombre = "ejercicio2"
+console.log(nombre)
+
+let invitados = ejercicio2.filter(invitados=>invitados.esFamiliar == true && invitados.edad >=18);
+console.log(invitados);
+
+
+
+function fibonacci(numero)
+  {
+    let numeros=[1, 1];
+    for (let  i = 2; i < numero; i++) {
+        numeros[i] = numeros[i - 2] + numeros[i-1];
+    }
+    return numeros;
+  }
+
+  console.log(fibonacci(50));
